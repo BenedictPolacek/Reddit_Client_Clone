3c1873dca3e2e0f7608e37d646e16f9e48afe797
Benedict Polacek <polacek.benedict@gmail.com>
2025-04-13 11:15:59 +0200

    create background for entire website

---
+++ src/components/background/background.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './background-style.css'
+
+
+export default function Background() {
+  return (
+    <div id="background">
+        <div id='stars'></div>
+        <div id='stars2'></div>
+        <div id='stars3'></div>
+    </div>
+  )
+}
