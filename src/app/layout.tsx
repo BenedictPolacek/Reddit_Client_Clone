fa7fee4216212dc7f602b96e26826614b47e7f01
Benedict Polacek <>
2025-04-23 19:32:40 +0200

    chore: remove unused font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono, Merriweather } from "next/font/google";
+import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
 import Background from "@/components/background/Background";
@@ -13,11 +13,6 @@ const geistSans = Geist({
 
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
-const merriWeather = Merriweather({
-  weight: '400',
   subsets: ["latin"],
 });
 
