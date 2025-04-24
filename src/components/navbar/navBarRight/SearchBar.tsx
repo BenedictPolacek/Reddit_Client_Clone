9dd9c9c7f684637705b4805ba6bef20d335852c7
Benedict Polacek <>
2025-04-24 15:15:16 +0200

    feat: implement search behavior to update URL search params

--- src/components/navbar/navBarRight/SearchBar.tsx
+++ src/components/navbar/navBarRight/SearchBar.tsx
@@ -1,8 +1,9 @@
 'use client'
+import Form from 'next/form'
 
 export default function SearchBar() {
   return (
-    <form className="hidden max-w-md mx-auto w-62 md:w-70 sm:block lg:w-100"> 
+    <Form action="" className="hidden max-w-md mx-auto w-62 md:w-70 sm:block lg:w-100"> 
         <label htmlFor="default-search" className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
         <div className="relative">
             <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
@@ -10,9 +11,9 @@ export default function SearchBar() {
                     <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
                 </svg>
             </div>
-            <input type="search" id="default-search" className="h-12 md:h-13 block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-900 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search posts..." required />
+            <input name="search" id="default-search" className="h-12 md:h-13 block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-900 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search posts..." required/>
             <button type="submit" className="text-white absolute end-2.5 bottom-1.5 md:bottom-2 bg-blue-600 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:ring-gray-700">Search</button>
         </div>
-    </form>
+    </Form>
   )
 }
