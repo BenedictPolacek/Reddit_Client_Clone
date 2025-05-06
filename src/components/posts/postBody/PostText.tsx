4e95e9ddcb75cdc2e9e9cab5ec922571d89b90a0
Benedict Polacek <polacek.benedict@gmail.com>
2025-05-07 00:10:26 +0200

    chore: update layout components and improve post styles
    - Updated BackgroundSky, PostLayout, and topics data
    - Styled post body and Header using Tailwind CSS
    - Cleaned up code formatting

--- src/components/posts/postBody/PostText.tsx
+++ src/components/posts/postBody/PostText.tsx
@@ -1,3 +1,6 @@
 export default function PostText({text}: {text?: string}) {
-    return text ? <p className="font-normal text-gray-700 dark:text-gray-400">{text}</p> : <></>
+  return (
+    text ? <p className="font-normal text-gray-700 dark:text-gray-400 overflow-hidden">{text}</p> 
+    : <></>
+  )
 }
--- src/components/posts/postBody/PostTitle.tsx
+++ src/components/posts/postBody/PostTitle.tsx
@@ -2,13 +2,13 @@ import { ViewRef } from "@/hooks/useMultipleInViews";
 
 export default function PostTitle({title, thumbnailUrl, lastRef}:{title: string, thumbnailUrl?: string, lastRef?: ViewRef}) {
   return (
-    <div ref={lastRef} >
+    <div ref={lastRef} className="overflow-hidden">
       {
         thumbnailUrl 
         ? <img src={thumbnailUrl} className="float-right rounded-md m-1"/>
         : <></>
       }
-      <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white overflow-hidden">
+      <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white ">
         {title}
       </h5>
     </div>
