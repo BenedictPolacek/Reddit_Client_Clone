945033a8d8040d149fffbe7fb918f6eccd9244d1
Benedict Polacek <>
2025-05-10 00:29:01 +0200

    refactor: move useInView to TopicItem and restructure logic to handle visibility changes and also track search term

--- src/app/[topic]/page.tsx
+++ src/app/[topic]/page.tsx
@@ -3,22 +3,21 @@ import PostLayout from "@/components/posts/PostLayout";
 import LoadingLayout from "@/components/loading/LoadingLayout";
 import { use } from "react";
 import { searchForEndpoint } from "@/utils/topicUtils";
-import { useMultipleInViews } from "@/hooks/useMultipleInViews";
 import useGetData from "@/hooks/useGetData";
+import { useInViewState } from "@/hooks/useInViewStates";
 
 type Params = Promise<{ topic: string }>
 type SearchParams = Promise<{ [term: string]: string | undefined }>
 
 export default function Home({params, searchParams}:{params: Params, searchParams: SearchParams}) {
   const { topic } = use(params);
-  const { term: searchTerm } = use(searchParams)
-  const topicEndpoint = searchForEndpoint(decodeURIComponent(topic));
-  const inViews = useMultipleInViews(2);
+  const { term: searchTerm } = use(searchParams);
+  const topicEndpoint = searchForEndpoint(decodeURIComponent(topic))
+  const { inViews, inViewHandler, inViewCount} = useInViewState(2)
+  const { data, initialFetch, isFetching, isUninitialized } = useGetData(topicEndpoint, searchTerm, inViews);
   
-  const { data, initialFetch, isFetching, isUninitialized, viewRefs } = useGetData(topicEndpoint, searchTerm, inViews);
-
   if (initialFetch || isUninitialized) return <LoadingLayout/>
   if (!data) throw Error('No data returned');
-  return <PostLayout Posts={data} viewRefs={viewRefs} isFetching={isFetching}/>
+  return <PostLayout Posts={data} isFetching={isFetching} inViewHandler={inViewHandler} inViewCount={inViewCount}/>
 }
 
--- src/components/posts/postBody/PostTitle.tsx
+++ src/components/posts/postBody/PostTitle.tsx
@@ -1,11 +1,10 @@
-import { ViewRef } from "@/hooks/useMultipleInViews";
 
-export default function PostTitle({title, thumbnailUrl, lastRef}:{title: string, thumbnailUrl?: string, lastRef?: ViewRef}) {
+export default function PostTitle({title, thumbnailUrl, lastRef}:{title: string, thumbnailUrl?: string, lastRef?: (node?: Element | null) => void}) {
   return (
     <div ref={lastRef} className="overflow-hidden">
       {
         thumbnailUrl 
-        ? <img src={thumbnailUrl} className="float-right rounded-md m-1"/>
+        ? <picture><img src={thumbnailUrl} alt='Post thumbnail' className="float-right rounded-md m-1"/></picture>
         : <></>
       }
       <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white ">
