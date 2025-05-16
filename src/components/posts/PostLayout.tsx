1242f49e3731652c12c45b12203f70d5d3e53bc4
Benedict Polacek <polacek.benedict@gmail.com>
2025-05-16 12:33:15 +0200

    chore: ensure consistent post width with Tailwind, fix useEffect deps, and adjust PostLayout data display

--- src/components/posts/PostLayout.tsx
+++ src/components/posts/PostLayout.tsx
@@ -4,25 +4,25 @@ import { getHalf } from "@/utils/postUtils"
 import { JSX } from "react";
 
 export default function PostLayout({data, isFetching}: { data: JSX.Element[] | null, isFetching: boolean}) {
-  const windowWidth = useWindowWidth()
-  const FirstHalf = data ? getHalf(data, 1):null
-  const SecondHalf = data ? getHalf(data, 2):null
+  const windowWidth = data !== null ? useWindowWidth() : null
+  const FirstHalf = data !== null ? getHalf(data, 1) : null
+  const SecondHalf = data !== null ? getHalf(data, 2) : null
   const lg = 1024;
   return (
     <>
-      <div className="hidden lg:flex justify-center">
-        <div className="flex-col justify-items-end basis-2/5 grow-0 m-4">
-          {data && windowWidth >= lg ? FirstHalf : null}
+      <div className="hidden lg:flex justify-center w-full">
+        <div className="flex-col justify-items-end basis-2/5 grow-0 m-4 overflow-hidden">
+          {windowWidth && data && windowWidth >= lg ? FirstHalf : null}
           {isFetching ? <Skeleton/> : null}
         </div>
-        <div className="flex-col justify-items-start basis-2/5 grow-0 m-4">
-          {data && windowWidth >= lg ? SecondHalf : null}
+        <div className="flex-col justify-items-start basis-2/5 grow-0 m-4 overflow-hidden">
+          {windowWidth && data && windowWidth >= lg ? SecondHalf : null}
           {isFetching ? <Skeleton/> : null}
         </div>
       </div>
       <div className="flex lg:hidden justify-center">
         <div className="flex flex-col basis-4/5 grow-0 items-center">
-          {data && windowWidth < lg ? data : null}
+          {windowWidth && data && windowWidth < lg ? data : null}
           {isFetching ? <Skeleton/> : null}
         </div>
       </div>
