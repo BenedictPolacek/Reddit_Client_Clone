546a1bbc845e0ab6a96f16c14d6dfbb94720270e
Benedict Polacek <>
2025-05-19 00:17:51 +0200

    test: set up Jest and add unit test for BackgroundSky component

--- src/components/background/BackgroundSky.tsx
+++ src/components/background/BackgroundSky.tsx
@@ -2,13 +2,13 @@
 import './backgroundStyle.css';
 import { useWindowWidth } from '@/hooks/useWindowWidth';
 
-export default function Background() {
+export default function BackgroundSky() {
   const backgroundWidth = 1878;
-  const windowWidth = useWindowWidth(backgroundWidth * 2)
+  const windowWidth = useWindowWidth(backgroundWidth * 4)
   const bgDifference = Math.ceil( windowWidth / backgroundWidth);
   const bgArray = Array.from({ length: bgDifference }, (_, i) => {
     return (
-      <div id="background" key={`bg-${i}`} style={{ left: `${i * 1878}px`, top: 0 }}>
+      <div id="background" data-testid='background-sky' key={`bg-${i}`} style={{ left: `${i * 1878}px`, top: 0 }}>
         <div id='stars'></div>
         <div id='stars2'></div>
         <div id='stars3'></div>
