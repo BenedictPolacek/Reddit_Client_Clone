05c04410a04fd0853695ae34804b75fce4bd01f6
Benedict Polacek <polacek.benedict@gmail.com>
2025-05-29 00:01:47 +0200

    test: add integration tests for entire Navbar

--- src/components/navbar/NavBarLeft.tsx
+++ src/components/navbar/NavBarLeft.tsx
@@ -6,6 +6,7 @@ export default function LeftNavbarSide() {
       href="https://benedictpolacek.github.io/BenedictPolacek-Personal-Portfolio-Website/" 
       className="text-white h-12 md:h-13 bg-[#050708] hover:bg-[#050708]/90 focus:ring-4 focus:outline-none focus:ring-[#050708]/50 
         font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-[#050708]/50 dark:hover:bg-[#050708]/30 me-2"
+      data-testid='author-brand'
     >
       <span className="hidden text-[15.5px] md:text-[17.5px] xs:block">Benedict Polacek</span>
       <span className="block text-[15px] xs:hidden">BP</span>
